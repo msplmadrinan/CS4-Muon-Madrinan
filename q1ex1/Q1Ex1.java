49253d6092fd96ca76d2ec1fdf938b9e1d41ff7e
msplmadrinan <96500044+msplmadrinan@users.noreply.github.com>
2022-09-06 08:59:00 +0800

    Add files via upload

---
+++ q1ex1/Q1Ex1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1ex1;
+
+/**
+ *
+ * @author MUON
+ */
+public class Q1Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name = "Paris Madrinan";
+        System.out.println("Hi, my name is " + name + ".");
+        int num = 16;
+        System.out.println("I am " + num + " years old.");
+        String section = "Muon";
+        System.out.println("My section is " + section + ".");
+    }
+}
