0de4001a48e7852e01075457c26854d1363f3d9f
msplmadrinan <96500044+msplmadrinan@users.noreply.github.com>
2022-09-12 08:58:00 +0800

    Add files via upload

---
+++ Q1Ex2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1ex2;
+
+/**
+ *
+ * @author MUON
+ */
+import java.util.Scanner;
+public class Q1Ex2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Welcome to Higher or Lower! \n Start game \n Change settings \n End application");
+       Scanner myObj = new Scanner(System.in);
+       String choice;
+       
+       System.out.println("What will you do?"); 
+       choice = myObj.nextLine();
+       
+    }
+    
+}
