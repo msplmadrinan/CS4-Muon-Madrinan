b76d5ac58d673eb18583f875ec3bd730fe1fec6f
msplmadrinan <96500044+msplmadrinan@users.noreply.github.com>
2022-09-15 11:34:54 +0800

    Add files via upload

--- Q1Ex2.java
+++ Q1Ex2.java
@@ -16,13 +16,34 @@ public class Q1Ex2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Welcome to Higher or Lower! \n Start game \n Change settings \n End application");
-       Scanner myObj = new Scanner(System.in);
-       String choice;
-       
-       System.out.println("What will you do?"); 
-       choice = myObj.nextLine();
-       
-    }
-    
-}
+      Scanner sc = new Scanner(System.in);
+      int range = 10;
+      int min = 1;
+      int guesses = 5;
+      String response;
+        do {System.out.println("Welcome to Higher or Lower! What will you do? \n"
+               + "- Start game \n"
+               + "- Change settings \n"
+               + "- End application");
+            response = sc.nextLine();
+            switch(response) {
+               case "Start game":
+                int random = (int) Math.floor(Math.random()*range) + min;
+                for (int remaining=guesses;
+                    remaining>0; remaining--){
+                    System.out.printf("You have id guess(es) left. What is your guess?", remaining);
+                    int answer = Integer.parseInt (sc.nextLine());
+                    if (answer==random) {System.out.println("You win!");
+                    break;
+                    }
+                    }
+            break; 
+            case "Change settings": } 
+            } while(repeat.equals("y"));
+            System.out.print("Play again (y/n)?");
+            
+            
+            {
+             
+             while(!response.equalsIgnoreCase ("End application"));
+            }
